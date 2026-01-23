f818ce196e7afd22dce1b22127e8068299a76407
mistr <michal.stransky42@gmail.com>
2026-01-24 00:47:35 +0100

    routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,19 @@
 // import { Button } from "@/components/ui/button"
 import { Route, Routes } from "react-router"
 import LoginPage from "./pages/login/page"
+import { CataloguePage } from "./pages/library/catalogue"
+import { BorrowPage } from "./pages/library/borrow"
 function App() {
     return (
         <Routes>
-            <Route path="/login" element={<LoginPage />} />
+            <Route path="/auth/login" element={<LoginPage />} />
+            <Route path="/admin">
+                <Route path="/books" element={<LoginPage />} />
+                <Route path="/readers" element={<LoginPage />} />
+                <Route path="/" element={<LoginPage />} />
+            </Route>
+            <Route path="/borrow" element={<BorrowPage />} />
+            <Route path="/catalogue" element={<CataloguePage />} />
             <Route path="/" element={<p>Root</p>} />
         </Routes>
     )
