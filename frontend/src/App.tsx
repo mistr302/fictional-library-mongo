c6405083253ca76c247572a20688821d59d23b31
mistr <michal.stransky42@gmail.com>
2026-01-24 01:32:22 +0100

    admin fe

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,14 +3,15 @@ import { Route, Routes } from "react-router"
 import LoginPage from "./pages/login/page"
 import { CataloguePage } from "./pages/library/catalogue"
 import { BorrowPage } from "./pages/library/borrow"
+import AdminSidebar from "./pages/admin/sidebar"
 function App() {
     return (
         <Routes>
             <Route path="/auth/login" element={<LoginPage />} />
             <Route path="/admin">
-                <Route path="/books" element={<LoginPage />} />
-                <Route path="/readers" element={<LoginPage />} />
-                <Route path="/" element={<LoginPage />} />
+                <Route path="books" element={<AdminSidebar />} />
+                <Route path="readers" element={<LoginPage />} />
+                <Route path="" element={<LoginPage />} />
             </Route>
             <Route path="/borrow" element={<BorrowPage />} />
             <Route path="/catalogue" element={<CataloguePage />} />
---
+++ frontend/src/components/app-sidebar.tsx
@@ -0,0 +1,109 @@
+import * as React from "react"
+import { ChevronRight, Icon } from "lucide-react"
+
+import { SearchForm } from "@/components/search-form"
+import { VersionSwitcher } from "@/components/version-switcher"
+import {
+    Collapsible,
+    CollapsibleContent,
+    CollapsibleTrigger,
+} from "@/components/ui/collapsible"
+import {
+    Sidebar,
+    SidebarContent,
+    SidebarGroup,
+    SidebarGroupContent,
+    SidebarGroupLabel,
+    SidebarHeader,
+    SidebarMenu,
+    SidebarMenuButton,
+    SidebarMenuItem,
+    SidebarRail,
+} from "@/components/ui/sidebar"
+import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar"
+import { AvatarBadge } from "./ui/avatar"
+
+// This is sample data.
+const data = {
+    navMain: [
+        {
+            title: "CRUD Administration",
+            url: "#",
+            items: [
+                {
+                    title: "Books",
+                    url: "#",
+                },
+                {
+                    title: "Readers",
+                    url: "#",
+                },
+            ],
+        },
+        {
+            title: "Application",
+            url: "#",
+            items: [
+                {
+                    title: "Catalogue",
+                    url: "#",
+                },
+                {
+                    title: "Active Loans",
+                    url: "#",
+                    isActive: true,
+                },
+            ],
+        },
+    ],
+}
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+    return (
+        <Sidebar {...props}>
+            <SidebarHeader>
+                <div className="flex flex-col">
+                    Authenticated as Admin
+                    Logout
+                </div>
+            </SidebarHeader>
+            <SidebarContent className="gap-0">
+                {/* We create a collapsible SidebarGroup for each parent. */}
+                {data.navMain.map((item) => (
+                    <Collapsible
+                        key={item.title}
+                        title={item.title}
+                        defaultOpen
+                        className="group/collapsible"
+                    >
+                        <SidebarGroup>
+                            <SidebarGroupLabel
+                                asChild
+                                className="group/label text-sidebar-foreground hover:bg-sidebar-accent hover:text-sidebar-accent-foreground text-sm"
+                            >
+                                <CollapsibleTrigger>
+                                    {item.title}{" "}
+                                    <ChevronRight className="ml-auto transition-transform group-data-[state=open]/collapsible:rotate-90" />
+                                </CollapsibleTrigger>
+                            </SidebarGroupLabel>
+                            <CollapsibleContent>
+                                <SidebarGroupContent>
+                                    <SidebarMenu>
+                                        {item.items.map((item) => (
+                                            <SidebarMenuItem key={item.title}>
+                                                <SidebarMenuButton asChild isActive={item.isActive}>
+                                                    <a href={item.url}>{item.title}</a>
+                                                </SidebarMenuButton>
+                                            </SidebarMenuItem>
+                                        ))}
+                                    </SidebarMenu>
+                                </SidebarGroupContent>
+                            </CollapsibleContent>
+                        </SidebarGroup>
+                    </Collapsible>
+                ))}
+            </SidebarContent>
+            <SidebarRail />
+        </Sidebar>
+    )
+}
